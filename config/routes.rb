cdc011d92478cba1f505d2ba147df6da130dafad
Yangzi <yangzihe12@gmail.com>
2015-03-13 23:51:17 -0700

    I'M DONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :dogs
+  # post "dogs/new", to: "dogs#create"
+
   get "users", to: "users#index"
   get "users/new", to: "users#new"
   get "users/:id", to: "users#show", as: :user
   post "users", to: "users#create"
+
 end
